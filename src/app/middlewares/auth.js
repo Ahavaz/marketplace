03fe41c4f63d849af9fa6f17158a6d14d139d60b
Ahavaz <lucas.cesar.8@hotmail.com>
2018-12-06 01:56:53 -0200

    Add authentication middleware

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const { secret } = require('../../config/auth')
+const { promisify } = require('util')
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, secret)
+
+    req.userId = decoded.id
+
+    return next()
+  } catch (err) {
+    return res.status(400).json({ error: 'Invalid token' })
+  }
+}
